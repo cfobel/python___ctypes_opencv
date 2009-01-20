e393d2567c8fa63d8de013ec8f5e177ee09d57cb
pmtri80 <pmtri80@7d674749-4f56-0410-b1b8-d5180b90564b>
2009-01-20 14:03:12 +0000

    improved pil_to_ipl() and ipl_to_pil()
    git-svn-id: http://ctypes-opencv.googlecode.com/svn/trunk@199 7d674749-4f56-0410-b1b8-d5180b90564b

--- src/opencv/interfaces.py
+++ src/opencv/interfaces.py
@@ -49,21 +49,22 @@ except ImportError:
 # PIL -- by Jeremy Bethmont
 #-----------------------------------------------------------------------------
 try:
-    import PIL
+    from PIL import Image
+    from cv import cvCvtColor, CV_RGB2BGR
 
     def pil_to_ipl(im_pil):
-        im_ipl = cvCreateImage(cvSize(im_pil.size[0], im_pil.size[1]),
+        im_ipl = cvCreateImageHeader(cvSize(im_pil.size[0], im_pil.size[1]),
 IPL_DEPTH_8U, 3)
-        im_ipl._owner = 1 # own header only
         data = im_pil.tostring('raw', 'RGB', im_pil.size[0] * 3)
         cvSetData(im_ipl, cast(data, POINTER(c_byte)), im_pil.size[0] * 3)
         cvCvtColor(im_ipl, im_ipl, CV_RGB2BGR)
-        return cvCloneImage(im_ipl)
+        im_ipl._depends = (data,)
+        return im_ipl
 
     def ipl_to_pil(im_ipl):
         size = (im_ipl.width, im_ipl.height)
         data = im_ipl.data_as_string()
-        im_pil = PIL.Image.fromstring(
+        im_pil = Image.fromstring(
                     "RGB", size, data,
                     'raw', "BGR", im_ipl.widthStep
         )
